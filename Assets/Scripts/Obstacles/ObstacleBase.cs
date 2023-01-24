045bf2fe31b0106fc173ca9cc7df80ba7f07207e
Gustavo Hitzschky <gust.campos@gmail.com>
2023-01-24 08:19:30 -0300

    Adjusting obstacles position

--- Assets/Scripts/Obstacles/ObstacleBase.cs
+++ Assets/Scripts/Obstacles/ObstacleBase.cs
@@ -6,6 +6,13 @@ public class ObstacleBase : MonoBehaviour {
 
     public float speed = .3f;
 
+    [SerializeField]
+    private float _variationYposition;
+
+    void Awake() {
+        transform.Translate(Vector3.up * Random.Range(-_variationYposition, _variationYposition));
+    }
+
     void Update() {
         transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
