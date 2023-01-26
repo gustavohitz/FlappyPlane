083bf6b81461a865a31237eb2323ca637d96c1ad
Gustavo Hitzschky <gust.campos@gmail.com>
2023-01-26 08:56:44 -0300

    Adding click icon when starting the game

---
+++ Assets/Scripts/Animation/HandAnimationController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnimationController : MonoBehaviour {
+
+    private SpriteRenderer _handImage;
+
+    void Awake() {
+        _handImage = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        HideAfterClicking();
+    }
+
+    void HideAfterClicking() {
+        if(Input.GetButtonDown("Fire1")) {
+            _handImage.enabled = false;
+        }
+    }
+
+}
